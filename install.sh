67568db6e1798501b8ff0530a49fc386087c6e40
Pascal <pascal@serveurperso.com>
2019-06-25 16:47:24 +0200

    Dependencies management

--- install.sh
+++ install.sh
@@ -10,24 +10,21 @@ fgrep bcm2835-v4l2 /etc/modules || echo bcm2835-v4l2 >> /etc/modules
 
 apt install -y nodejs npm ffmpeg
 
+rm -rf $BASEDIR
 mkdir -p $BASEDIR
 cd $BASEDIR
 
-npm install socket.io-client stream-split rpio rpio-pwm
-
-rm -f clientrobotp.js
-rm -f trame.js
-
-wget $BASEURL/clientrobotpi.js -P $BASEDIR -N
-wget $BASEURL/trame.js -P $BASEDIR -N
+wget $BASEURL/package.json
+wget $BASEURL/clientrobotpi.js
+wget $BASEURL/trame.js
 
 wget $BASEURL/robot.json -P /boot -N
 wget $BASEURL/vigiclient.service -P /etc/systemd/system -N
 
-rm -f processdiffusion
-rm -f processdiffaudio
-
 ln -s /bin/cat $BASEDIR/processdiffusion
 ln -s $(which ffmpeg || echo ffmpegnotfound) $BASEDIR/processdiffaudio
+
+#npm install socket.io-client stream-split rpio rpio-pwm
+npm install
 
 systemctl enable vigibot
