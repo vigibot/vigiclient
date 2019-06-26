11bc0594f2a2123236184d62197c041042ff8871
Pascal <pascal@serveurperso.com>
2019-06-26 10:04:56 +0200

    Installation subdirectory

--- vigiupdate.sh
+++ vigiupdate.sh
@@ -5,5 +5,5 @@ set -u
 
 BASEDIR=/usr/lib/vigiclient
 
-wget https://www.vigibot.com/clientrobotpi.js -P $BASEDIR -N
-wget https://www.vigibot.com/trame.js -P $BASEDIR -N
+wget https://www.vigibot.com/vigiclient/clientrobotpi.js -P $BASEDIR -N
+wget https://www.vigibot.com/vigiclient/trame.js -P $BASEDIR -N
