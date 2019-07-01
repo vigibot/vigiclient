d863ab14eecedb047149b07f153f7c83679b27c3
Pascal <pascal@serveurperso.com>
2019-07-01 11:20:00 +0200

    Self-updating updater and node dependency update

--- vigiupdate.sh
+++ vigiupdate.sh
@@ -6,23 +6,40 @@ set -u
 BASEURL=https://www.vigibot.com/vigiclient
 BASEDIR=/usr/local/vigiclient
 
-restart=no
+updated=no
 
 function check() {
- before=$(date -r $BASEDIR/$1 +%s)
+ before=$(date -r $BASEDIR/$1 +%s || echo 0)
  wget $BASEURL/$1 -P $BASEDIR -N
  after=$(date -r $BASEDIR/$1 +%s)
 
  if [ $before != $after ]
  then
-  restart=yes
+  updated=yes
  fi
 }
+
+check vigiupdate.sh
+
+if [ $updated == "yes" ]
+then
+ exit 0
+fi
+
+check package.json
+
+if [ $updated == "yes" ]
+then
+ cd $BASEDIR
+ rm -rf node_modules
+ npm install
+ exit 0
+fi
 
 check clientrobotpi.js
 check trame.js
 
-if [ $restart == "yes" ]
+if [ $updated == "yes" ]
 then
  systemctl restart vigiclient
 fi
