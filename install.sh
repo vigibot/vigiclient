c6fb7449c0c6c6137a5085fde2b688dbce4950ac
Pascal <pascal@serveurperso.com>
2019-07-03 13:32:20 +0200

    Minor syntax modification

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set -u
 BASEURL=https://www.vigibot.com/vigiclient
 BASEDIR=/usr/local/vigiclient
 
-if [[ $EUID -ne 0 ]]
+if [ $EUID -ne 0 ]
 then
  echo "This script must be run as root" 1>&2
  exit 1
