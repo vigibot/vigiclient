36d976274d04d07642f26e090d3ebfcf7c232556
Pascal <pascal@serveurperso.com>
2019-07-04 18:52:22 +0200

    Matching uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -11,15 +11,30 @@ then
  exit 1
 fi
 
+echo "Disable I2C"
+sed -i "s/dtparam=i2c_arm=on/#dtparam=i2c_arm=on/" /boot/config.txt
+sed -i "/i2c-dev/d" /etc/modules
+
+echo "Disable camera"
+sed -i "/start_x=1/d" /boot/config.txt
+sed -i "/gpu_mem=128/d" /boot/config.txt
+
+echo "Disable Video4Linux"
 sed -i "/bcm2835-v4l2/d" /etc/modules
 
+echo "Service uninstall"
 systemctl stop vigiclient
 systemctl disable vigiclient
 rm -f /etc/systemd/system/vigiclient.service
 
-rm -rf $BASEDIR
-
-rm -f /boot/robot.json
+echo "Updater uninstall"
 rm -f /etc/cron.d/vigicron
 
-echo sudo apt remove --purge nodejs npm ffmpeg
+echo "Removing the config file"
+rm -f /boot/robot.json
+
+echo "Cleaning"
+rm -rf $BASEDIR
+
+echo "To uninstall FFmpeg and Node.js please do:"
+echo "sudo apt remove --purge ffmpeg npm"
