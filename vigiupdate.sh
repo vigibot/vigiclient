0c2a013705b2486020c588d5de93ca5c6dabe63d
Pascal <pascal@serveurperso.com>
2020-03-12 15:29:01 +0100

    Capturing abnormal script termination

--- vigiupdate.sh
+++ vigiupdate.sh
@@ -14,7 +14,7 @@ trace() {
 
 abnormal() {
  trace "Abnormal script termination"
- wget $BASEURL/abnormal.php -O /dev/null -T 5 -t 5 > /dev/null 2>&1
+ wget https://www.vigibot.com/abnormal.php -O /dev/null -T 5 -t 5 > /dev/null 2>&1
 }
 
 check() {
