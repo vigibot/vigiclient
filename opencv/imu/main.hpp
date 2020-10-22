17693df551a3ebc2708c8ea0041f9064ad22277e
Pascal <pascal@serveurperso.com>
2020-10-22 15:05:13 +0200

    Minor

--- opencv/imu/main.hpp
+++ opencv/imu/main.hpp
@@ -8,14 +8,14 @@
 #define OFFSETX M_PI
 #define OFFSETY 0.0
 #define OFFSETZ 0.0
-#define MARGIN 5
-#define DIAM1 20
-#define DIAM2 10
-#define COEF2 4.0
 
 #define DIVVZ 20
 #define KPVZ 3
 #define KDVZ 6
+#define MARGIN 5
+#define DIAM1 20
+#define DIAM2 10
+#define COEF2 4.0
 
 int width;
 int height;
