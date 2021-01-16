f0b79123032b2f16ba8d34bc85530296b2c8ead7
Pascal <pascal@serveurperso.com>
2021-01-16 09:48:42 +0100

    Correct automatic zoom for fixed map views

--- opencv/lidar/main.cpp
+++ opencv/lidar/main.cpp
@@ -1015,7 +1015,7 @@ void ui(Mat &image, vector<Point> &robotPoints, vector<Line> robotLinesAxes[], v
   }
 
   offsetPoint = Point(xmax + xmin, ymax + ymin) / 2;
-  mapDivFixed = constrain(max((xmax - xmin) / width, (ymax - ymin) / height) * 10, MAPDIVMIN, MAPDIVMAX);
+  mapDivFixed = constrain(max((xmax - xmin) / width, (ymax - ymin) / height) * 11, MAPDIVMIN, MAPDIVMAX);
  }
 
  if(!nodes.empty()) {
